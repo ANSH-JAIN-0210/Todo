a4aa47df8c7cb3a2419c38c60b12efd7054eb170
ANSH JAIN <106751193+ANSH-JAIN-0210@users.noreply.github.com>
2025-04-02 23:04:26 +0530

    cors access

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,27 @@ const userRoutes = require("./routes/UserRoutes");
 const todoRoutes = require("./routes/TodoRoutes");
 
 const app = express();
-const corsOptions = {
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization"
-};
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "https://todo-frontend-e4l4.onrender.com/",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/todoApp', {
+mongoose.connect("mongodb://localhost:27017/todoApp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Connected to MongoDB"));
 
-app.use('/api', userRoutes);
-app.use('/api', todoRoutes);
+app.use("/api", userRoutes);
+app.use("/api", todoRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
